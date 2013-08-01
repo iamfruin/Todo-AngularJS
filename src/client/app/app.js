8f2171a5e7633e3830957bb201612ed1a96d13b5
Craig Fruin <cfruin@gmail.com>
2013-07-31 22:07:54 -0600

    reorganizing project

---
+++ src/client/app/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('todoListApp', []);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/todo',
+            {
+                controller: 'TodoController',
+                templateUrl: '/app/partials/todo.html'
+            })
+        .when('/contact',
+            {
+                templateUrl: '/app/partials/contact.html'
+            })
+        .otherwise({ redirectTo: '/todo' });
+});
+
+
+
+
